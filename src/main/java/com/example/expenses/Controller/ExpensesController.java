4da9512174c24a36b9a02f1ced4984f8cdb83f4a
akash jain <jainakash43@gmail.com>
2022-06-11 14:39:25 +0530

    updating home branch

--- src/main/java/com/example/expenses/Controller/ExpensesController.java
+++ src/main/java/com/example/expenses/Controller/ExpensesController.java
@@ -61,7 +61,7 @@ public class ExpensesController {
 	    return temp;
 	}
 	
-	@GetMapping("/getTotalExpensesofCurrentMonth")
+	@GetMapping("/getTotalExpensesOfCurrentMonth")
 	public int  getTotalExpensesofCurrentMonth()
 	{
 		int total = 0;
